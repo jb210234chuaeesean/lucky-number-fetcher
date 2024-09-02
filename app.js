37db2f4cff8f14255481ce23f2cc3ab40808c2e7
chuaeesean <89498631+chuaeesean@users.noreply.github.com>
2024-09-02 19:27:48 +0800

    modified:   app.js modified:   node_modules/.package-lock.json new file:   node_modules/cors/CONTRIBUTING.md new file:   node_modules/cors/HISTORY.md new file:   node_modules/cors/LICENSE new file:   node_modules/cors/README.md new file:   node_modules/cors/lib/index.js new file:   node_modules/cors/package.json new file:   node_modules/object-assign/index.js new file:   node_modules/object-assign/license new file:   node_modules/object-assign/package.json new file:   node_modules/object-assign/readme.md modified:   package-lock.json modified:   package.json

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const express = require('express');
-const app = express();
 const axios = require('axios');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
+const app = express();
 app.use(express.json());
 app.use(cors({origin: '*'}));
 app.disable('x-powered-by');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
 
 app.get('/query/:id', (req, res) => {
-    axios.get(`https://www.4dmoon.com/zh-cn/4d-history/${req.params.id}`)
-        .then(response => {
-        res.send(response.data);
-        })
-        .catch(error => {
-        console.log(error);
-        });
+    
 })
 
 app.listen(5000, () => {
-    
+    console.log('Server is running on port 5000');
 });
