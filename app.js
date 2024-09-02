38d81ad529e452476a90bdd72d1a24f586117687
chuaeesean <89498631+chuaeesean@users.noreply.github.com>
2024-09-02 18:12:06 +0800

    modified:   app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const app = express();
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.get('/query/:id', (req, res) => {
     axios.get(`https://www.4dmoon.com/zh-cn/4d-history/${req.params.id}`)
@@ -17,3 +18,7 @@ app.get('/query/:id', (req, res) => {
         console.log(error);
         });
 })
+
+app.listen(5000, () => {
+    
+});
